1ddb9df953ddfd5dcd3bdf2ca5e7599d83b37b03
esdra-mf <165320745+esdra-mf@users.noreply.github.com>
2024-03-28 19:26:42 -0300

    Arquivos do Site Adicionados

---
+++ app.js
@@ -0,0 +1,19 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-item');
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.classList.add('active');
+    } else {
+      slide.classList.remove('active');
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 8000); // Troca de slide a cada 8 segundos
